73d03a66289500a4afba7c971fdec6fa718c2b5c
prakhars557 <educationisfreedom.75938@gmail.com>
2023-11-11 20:37:03 +0530

    changes made on initial commmit

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def Helloworld():
+    return render_template('login.html')
     #return 'Hello, World!'
 
 @app.route('/products')
