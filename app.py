aa89c4f2111b3b9c7698dd3a2f65720efb25a816
prakhars557 <educationisfreedom.75938@gmail.com>
2023-11-12 21:28:01 +0530

    Part1 completed

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
-@app.route('/')
-def Helloworld():
-    return render_template('login.html')
-    #return 'Hello, World!'
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
 
-@app.route('/products')
-def products():
-    return 'this is products page'
+@app.route("/loginPage")
+def renderLogin():
+    return render_template("form.html")
 
-if __name__=="__main__":
+@app.route("/formlogin", methods=['POST'])
+def login():
+    userName = request.form["userName"]
+    password = request.form["password"]
+    if userName == 'prakhar' and password == '123':
+        return "<p>Successful Login</p>"
+    else:
+        return "Invalid User Name or Password"
+
+if __name__ == "__main__":
     app.run(debug=True)
